2e922242cf914c51cadf37a213717dc698e10893
Abhinav Verma <abhinav_win12@yahoo.co.in>
2019-01-24 00:53:16 +0530

    React router basics
    Basics of react Router, browserRouter, Navlink

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import "./styles.css";
+
+const PageOne = () => {
+  return (
+    <div>
+      Page One
+      <NavLink to="/pagetwo">Navigate to Page two</NavLink>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <NavLink to="/pageone">Navigate to Page one</NavLink>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router basename={"/v1"}>
+        <div>
+          <Route path="/pageone" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </Router>
     </div>
   );
 }
